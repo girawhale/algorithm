9d72f45db44192c1deefe8aad6975acf55a71db6
girawhale <girawhale@naver.com>
2021-02-01 20:39:23 +0900

    first commit

---
+++ src/prog67256_키패드_누르기.java
@@ -0,0 +1,28 @@
+public class prog67256_키패드_누르기 {
+    StringBuilder sb = new StringBuilder();
+    int prevL = 10, prevR = 12;
+
+    public String solution(int[] numbers, String hand) {
+        char handDefault = hand.equals("right") ? 'R' : 'L';
+        for (int n : numbers) {
+            if (n == 0 || n % 3 == 2) {
+                if (n == 0) n = 11;
+                int diff = getDiff(prevL, n) - getDiff(prevR, n);
+
+                push(n, diff < 0 ? 'L' : (diff > 0 ? 'R' : handDefault));
+            } else push(n, n % 3 == 1 ? 'L' : 'R');
+        }
+        return sb.toString();
+    }
+
+    void push(int n, char hand) {
+        sb.append(hand);
+
+        if (hand == 'L') prevL = n;
+        else prevR = n;
+    }
+
+    int getDiff(int prev, int num) {
+        return Math.abs(prev - num) / 3 + Math.abs(prev - num) % 3;
+    }
+}
---
+++ src/prog67257_수식_최대화.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class prog67257_수식_최대화 {
+    public long solution(String expression) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        new prog67257_수식_최대화().solution("100-200*300-500+20");
+    }
+}

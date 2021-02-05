cb6d198fc63bee5d5451534d12c02838a71d81f3
girawhale <girawhale@naver.com>
2021-02-06 03:35:29 +0900

    21.02.06 UPDATE 프로그래머스 광고삽입

--- src/prog72415_카드_짝_맞추기.java
+++ src/prog72415_카드_짝_맞추기.java
@@ -25,7 +25,8 @@ public class prog72415_카드_짝_맞추기 {
 
     void permutation(int cnt) {
         if (cnt > cardCnt) {
-            bfs();
+            ans = Math.min(ans, bfs());
+            System.out.println(ans);
             return;
         }
 
@@ -41,34 +42,64 @@ public class prog72415_카드_짝_맞추기 {
     int bfs() {
         Queue<int[]> queue = new LinkedList<>();
         boolean[][] visit = new boolean[4][4];
-        int idx = 0, ret = 0, cnt = 0;
+        int idx = 1, ret = 0, cnt = 0;
         queue.add(new int[]{r, c, 0});
+        System.out.println(Arrays.toString(perm));
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
+            System.out.println(Arrays.toString(cur));
 
             if (board[cur[0]][cur[1]] == perm[idx]) {
                 queue.clear();
                 visit = new boolean[4][4];
                 ret += cur[2];
+                visit[cur[0]][cur[1]] = true;
+                queue.add(new int[]{cur[0], cur[1], 0});
 
                 if (cnt == 1) {
                     cnt = 0;
                     idx++;
+                    if (idx > cardCnt) return ret;
                 }
+
+                if (ret >= ans) return Integer.MAX_VALUE;
+                continue;
             }
 
             for (int k = 0; k < 4; k++) {
                 int ny = cur[0] + dy[k];
                 int nx = cur[1] + dx[k];
 
-                if (ny < 0 || nx < 0 || ny >= 4 || nx >= 4) continue;
+                if (!(ny < 0 || nx < 0 || ny >= 4 || nx >= 4 || visit[ny][nx])) {
+                    visit[ny][nx] = true;
+                    queue.add(new int[]{ny, nx, cur[2] + 1});
+                }
 
+                while (true) {
+                    if (ny < 0 || nx < 0 || ny >= 4 || nx >= 4) {
+                        ny -= dy[k];
+                        nx -= dx[k];
+                        break;
+                    }
+                    ny += dy[k];
+                    nx += dx[k];
 
+                    if (board[ny][nx] != 0) break;
+                }
+
+                if (visit[ny][nx]) continue;
+                visit[ny][nx] = true;
+                queue.add(new int[]{ny, nx, cur[2] + 1});
             }
 
         }
 
-        return Integer.MAX_VALUE;
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new prog72415_카드_짝_맞추기()
+                .solution(new int[][]{{1, 0, 0, 3}, {2, 0, 0, 0}, {0, 0, 0, 2}, {3, 0, 1, 0}}, 1, 0));
     }
 }

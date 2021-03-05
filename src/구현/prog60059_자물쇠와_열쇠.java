00be9ce0cbf9cfa572cb1b53ef48ac4b64b49afd
girawhale <girawhale@naver.com>
2021-03-06 04:59:52 +0900

    21.03.06 UPDATE 프로그래머스 괄호변환, 자물쇠와 열쇠

---
+++ src/구현/prog60059_자물쇠와_열쇠.java
@@ -0,0 +1,37 @@
+package 구현;
+
+public class prog60059_자물쇠와_열쇠 {
+    int N, M;
+
+    public boolean solution(int[][] key, int[][] lock) {
+        M = key.length;
+        N = lock.length;
+
+        for (int k = 0; k < 4; k++) {
+            for (int i = -M; i < N + M; i++) {
+                for (int j = -M; j < N + M; j++)
+                    if (solve(key, lock, i, j)) return true;
+            }
+
+            int[][] rotate = new int[M][M];
+            for (int i = 0; i < M; i++) for (int j = 0; j < M; j++) rotate[i][j] = key[M - j - 1][i];
+            key = rotate;
+        }
+
+        return false;
+    }
+
+    boolean solve(int[][] key, int[][] lock, int y, int x) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int ky = i - y, kx = j - x, k;
+                if (ky < 0 || kx < 0 || ky >= M || kx >= M) k = 0;
+                else k = key[ky][kx];
+
+                if (lock[i][j] == k) return false;
+            }
+            System.out.println();
+        }
+        return true;
+    }
+}
---
+++ src/문자열/prog60058_괄호_변환.java
@@ -0,0 +1,34 @@
+package 문자열;
+
+public class prog60058_괄호_변환 {
+    public String solution(String p) {
+        return solve(p);
+    }
+
+    String solve(String s) {
+        if (s.length() == 0) return s;
+
+        int idx = 0, cnt = 0;
+        for (; idx < s.length(); idx++) {
+            cnt += s.charAt(idx) == '(' ? 1 : -1;
+            if (cnt == 0) break;
+        }
+        String u = s.substring(0, ++idx), v = solve(s.substring(idx));
+
+        if (check(u)) return u + v;
+        else
+            return "(" + v + ")" +
+                    u.substring(1, u.length() - 1).chars()
+                            .mapToObj(i -> i == '(' ? ')' : '(')
+                            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append);
+    }
+
+    boolean check(String str) {
+        int cnt = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') cnt++;
+            else if (--cnt < 0) return false;
+        }
+        return true;
+    }
+}

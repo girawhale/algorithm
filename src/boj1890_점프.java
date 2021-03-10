65a917332876576c2cee160b1ddcd0c8e1a56079
girawhale <girawhale@naver.com>
2021-03-11 01:42:03 +0900

    21.03.11 UPDATE 백준 점프

---
+++ src/boj1890_점프.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class boj1890_점프 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][N];
+        long[][] dp = new long[N][N];
+        for (int i = 0; i < N; i++)
+            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        dp[0][0] = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dp[i][j] == 0 || (i == N - 1 && j == N - 1)) continue;
+
+                int n;
+                if ((n = i + map[i][j]) < N) dp[n][j] += dp[i][j];
+                if ((n = j + map[i][j]) < N) dp[i][n] += dp[i][j];
+            }
+        }
+
+        System.out.println(dp[N - 1][N - 1]);
+    }
+}

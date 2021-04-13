6c78454ceee80f3f68687801802088ace932c010
girawhale <girawhale@naver.com>
2021-04-13 23:37:20 +0900

    21.04.13 UPDATE boj 물병

---
+++ src/비트마스킹/boj1052_물병.java
@@ -0,0 +1,20 @@
+package 비트마스킹;
+
+import java.util.Scanner;
+
+public class boj1052_물병 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), K = sc.nextInt();
+
+        int num = N;
+        while (true) {
+            int bitCount = Integer.bitCount(num);
+            if (bitCount <= K) {
+                System.out.println(num - N);
+                return;
+            }
+            num++;
+        }
+    }
+}

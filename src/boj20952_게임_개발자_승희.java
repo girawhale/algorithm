916e0e23bff93d8b34f2b2df387f0452519321a9
girawhale <girawhale@naver.com>
2021-04-30 00:44:42 +0900

    21.04.30 UPDATE

---
+++ src/boj20952_게임_개발자_승희.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class boj20952_게임_개발자_승희 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+
+        boolean[] remain = new boolean[7];
+        int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).peek(a -> remain[a % 7] = true).toArray();
+        int[] B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int count = (int) IntStream.range(0, 7).filter(i -> remain[i]).count();
+
+        long sum = 0;
+        for (int b : B) {
+            sum += b;
+            int idx = (int) ((7 - (sum % 7)) % 7);
+
+            if (!remain[idx]) continue;
+            if (count == 1) {
+                sum -= b;
+                continue;
+            }
+
+            remain[idx] = false;
+            count--;
+        }
+
+        List<Long> ans = new ArrayList<>();
+        final int MOD = (int) (1e9) + 7;
+        for (int a : A) {
+            if (!remain[a % 7]) continue;
+
+            ans.add((sum + a) % MOD);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(ans.size()).append('\n');
+        ans.forEach(i -> sb.append(i).append(' '));
+        System.out.println(sb);
+    }
+}
--- src/그리디/boj1339_단어_수학.java
+++ src/그리디/boj1339_단어_수학.java
@@ -1,30 +1,24 @@
 package 그리디;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class boj1339_단어_수학 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        String[] words = IntStream.range(0, N).mapToObj(i -> sc.next()).toArray(String[]::new);
-        Map<Character, Integer> map = new HashMap<>();
+        int[] count = new int[26];
 
-        for (String word : words) {
+        for (int i = 0; i < N; i++) {
+            String word = sc.next();
             int len = word.length();
-            for (int i = 0; i < len; i++) {
-                char ch = word.charAt(i);
-                map.put(ch, map.getOrDefault(ch, 0) + (int) Math.pow(10, len - i - 1));
-            }
-        }
-        List<Character> chList = new ArrayList<>(map.keySet());
-        chList.sort((o1, o2) -> Integer.compare(map.get(o2), map.get(o1)));
 
-        int sum = 0;
-        for (String word : words) {
-            sum += word.chars().reduce(0, (s, ch) -> s * 10 + (9 - chList.indexOf((char) ch)));
+            IntStream.range(0, len).forEach(j -> count[word.charAt(j) - 'A'] += (int) Math.pow(10, len - j - 1));
         }
+        Arrays.sort(count);
 
+        int sum = IntStream.range(0, 10).reduce(0, (s, i) -> s + count[25 - i] * (9 - i));
         System.out.println(sum);
     }
 }

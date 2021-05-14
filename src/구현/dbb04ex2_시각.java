9da71b57dce7389089aded5296536a0330a3a59c
girawhale <girawhale@naver.com>
2021-05-14 23:05:07 +0900

    ○, [구현] dbb04ex2_시각 이름 재지정

--- src/구현/dbb04ex2_시각.java
+++ src/구현/dbb04ex2_시각.java
@@ -2,7 +2,7 @@ package 구현;
 
 import java.util.Scanner;
 
-public class ddb04ex2_시각 {
+public class dbb04ex2_시각 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();

96074db4b2a0216b0067c8407533c7bbb5f902f3
girawhale <girawhale@naver.com>
2021-05-14 23:02:41 +0900

    ○, [구현] ddb0402_왕실의_나이트

---
+++ src/구현/ddb0402_왕실의_나이트.java
@@ -0,0 +1,25 @@
+package 구현;
+
+import java.util.Scanner;
+
+public class ddb0402_왕실의_나이트 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] dy = {-2, -2, 2, 2, -1, 1, -1, 1};
+        int[] dx = {-1, 1, -1, 1, -2, -2, 2, 2};
+
+        String in = sc.next();
+        int y = in.charAt(0) - 'a' + 1;
+        int x = in.charAt(1) - '0';
+
+        int ans = 0;
+        for (int k = 0; k < 8; k++) {
+            int ny = y + dy[k];
+            int nx = x + dx[k];
+
+            if (ny <= 0 || nx <= 0 || ny > 8 || nx > 8) continue;
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}

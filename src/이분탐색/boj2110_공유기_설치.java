5cf894e687d38354996b2e6e0415e21afb2b320d
girawhale <girawhale@naver.com>
2021-05-19 23:03:13 +0900

    ○, [이분탐색] boj2110_공유기_설치(dbb1529)

--- src/이분탐색/boj2110_공유기_설치.java
+++ src/이분탐색/boj2110_공유기_설치.java
@@ -6,37 +6,43 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class boj2110_공유기_설치 {
+public class boj2110_공유기_설치 { // dbb1529
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        int[] home = new int[N];
-        for (int i = 0; i < N; i++) home[i] = Integer.parseInt(br.readLine());
-        Arrays.sort(home);
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
 
         int s = 0, e = (int) 1e9, ans = 0;
 
         while (s <= e) {
             int m = (s + e) / 2;
-            int prev = home[0];
-            int cnt = 1;
 
-            for (int i = 1; i < N; i++) {
-                if (home[i] - prev >= m) {
-                    cnt++;
-                    prev = home[i];
-                }
-            }
-
-            if (cnt >= C) {
+            if (getCount(arr, m) >= C) {
                 ans = m;
                 s = m + 1;
             } else e = m - 1;
         }
-
         System.out.println(ans);
     }
+
+    static int getCount(int[] arr, int dist) {
+        int cnt = 1;
+        int prev = arr[0];
+
+        for (int a : arr) {
+            if (a - prev >= dist) {
+                cnt++;
+                prev = a;
+            }
+        }
+
+        return cnt;
+    }
+
+
 }

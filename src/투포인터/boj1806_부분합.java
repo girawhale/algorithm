0285343169048425b002524b5ac3bef454598c9b
girawhale <girawhale@naver.com>
2021-06-10 01:21:29 +0900

    ○,[투포인터] boj1806_부분합

--- src/투포인터/boj1806_부분합.java
+++ src/투포인터/boj1806_부분합.java
@@ -3,28 +3,32 @@ package 투포인터;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class boj1806_부분합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()), S = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] num = new int[N + 1];
 
-        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for (int i = 1; i < N; i++) nums[i] += nums[i - 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++)
+            num[i] += num[i - 1] + Integer.parseInt(st.nextToken());
 
-        int len = Integer.MAX_VALUE, s = 0, e = 0;
-        while (s < N && e < N) {
-            int sum = nums[e] - (s > 0 ? nums[s - 1] : 0);
-            if (sum >= S && len > e - s + 1) {
-                len = e - s + 1;
-            }
-            if (sum < S) e++;
-            else s++;
+        int left = 0, right = 1;
+        int ans = Integer.MAX_VALUE;
+
+        while (left <= N && right <= N) {
+            int sum = num[right] - num[left];
+            if (sum >= S)
+                ans = Math.min(ans, right - left);
+
+            if (sum < S) right++;
+            else left++;
         }
-
-        System.out.println(len == Integer.MAX_VALUE ? 0 : len);
+        System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
     }
 }
+

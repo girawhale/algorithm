7919f8e9380470e71c49bb18ac7a5c695063079d
girawhale <girawhale@naver.com>
2021-06-11 00:40:19 +0900

    ○,[투포인터] prog42885_구명보트

--- src/투포인터/prog42885_구명보트.java
+++ src/투포인터/prog42885_구명보트.java
@@ -5,18 +5,13 @@ import java.util.Arrays;
 public class prog42885_구명보트 {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
-        int left = 0, right = people.length - 1;
 
+        int left = 0, right = people.length - 1;
         int ans = 0;
         while (left < right) {
-            int sum = people[left] + people[right];
-
-            if (sum > limit) {
-                right--;
-            } else {
+            if (people[left] + people[right] <= limit)
                 left++;
-                right--;
-            }
+            right--;
             ans++;
         }
 

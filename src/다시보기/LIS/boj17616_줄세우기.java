f0df7fd874c5048d9f196a0341345704e36680a8
girawhale <girawhale@naver.com>
2021-06-15 17:25:46 +0900

    ○,[LIS] boj17616_줄세우기

---
+++ src/다시보기/LIS/boj17616_줄세우기.java
@@ -0,0 +1,36 @@
+package 다시보기.LIS;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class boj17616_줄세우기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = IntStream.range(0, N).map(i -> sc.nextInt()).toArray();
+
+        List<Integer> list = new ArrayList<>();
+        int[] lis = new int[N];
+
+        list.add(arr[0]);
+        lis[0] = 1;
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] <= list.get(list.size() - 1)) {
+                int idx = Collections.binarySearch(list, arr[i]);
+
+                if (idx >= 0) {
+                    lis[i] = idx + 1;
+                } else {
+                    idx = -idx - 1;
+                    list.set(idx, arr[i]);
+                    lis[i] = idx + 1;
+                }
+            } else {
+                list.add(arr[i]);
+                lis[i] = list.size();
+            }
+        }
+        System.out.println(N - list.size());
+    }
+}

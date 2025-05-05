100396ecc7f078c388e8c825ad720c4c67855397
girawhale <girawhale@naver.com>
2025-05-05 16:57:00 +0900

    ○,[문자열] leetcode28_find_the_index_of_the_first_occurrence_in_a_string

--- src/문자열/leetcode28_find_the_index_of_the_first_occurrence_in_a_string.java
+++ src/문자열/leetcode28_find_the_index_of_the_first_occurrence_in_a_string.java
@@ -4,4 +4,19 @@ public class leetcode28_find_the_index_of_the_first_occurrence_in_a_string {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
+
+    public int strStrWithWindow(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }

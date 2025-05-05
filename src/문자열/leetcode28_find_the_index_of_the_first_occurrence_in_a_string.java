01e9599b0d32aa9328bd898d75939e059156e56f
girawhale <girawhale@naver.com>
2025-05-05 16:51:41 +0900

    ○,[문자열] leetcode28_find_the_index_of_the_first_occurrence_in_a_string

---
+++ src/문자열/leetcode28_find_the_index_of_the_first_occurrence_in_a_string.java
@@ -0,0 +1,7 @@
+package 문자열;
+
+public class leetcode28_find_the_index_of_the_first_occurrence_in_a_string {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}

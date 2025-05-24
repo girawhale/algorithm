704555e162f6cfd60322272be119ce7e76e3f6cd
girawhale <girawhale@naver.com>
2025-05-24 18:47:07 +0900

    ○

---
+++ src/자료구조/Map/leetcode1_two_sum.java
@@ -0,0 +1,18 @@
+package 자료구조.Map;
+
+import java.util.*;
+
+public class leetcode1_two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
